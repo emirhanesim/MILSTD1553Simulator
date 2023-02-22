b5e5b56967b9b2f1d394be59292053302f1e8aa9
emirh <emirh@DESKTOP-DNG1DA0>
2023-02-22 19:56:02 +0300

    Add project files.

---
+++ MILSTD1553ClassLibrary/Message.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MILSTD1553ClassLibrary
+{
+    public class Message
+    {
+        private int CommandWord { get; set; }
+        private int DataWordArrayLength { get; set; }
+        private int[] DataWordArray { get; set; }
+        private float messageDuration;
+
+        //Constructor for BC-RT (RT receives) message
+        public Message(int commandWord, int[] dataWordArray)
+        {
+            this.CommandWord = commandWord;
+            this.DataWordArray = dataWordArray;
+            this.DataWordArrayLength = dataWordArray.Length;
+        }
+
+        //Constructor for RT-BC (RT transmits) message
+        public Message(int commandWord) 
+        {
+            this.CommandWord = commandWord;
+            this.DataWordArray = null;
+            this.DataWordArrayLength = 0;
+        }
+
+        //Method: getMessageDuration
+            //returns in millisecond
+            //word duration: 16 ms (16 bit x 1 ms)
+        public float getMessageDuration()
+        {
+            messageDuration = 0.001F * (1 + DataWordArrayLength);
+            return messageDuration;
+        }
+
+        
+        public int[] returnMessage()
+        {
+            int[] messageArray = new int[1 + DataWordArrayLength];
+            if (DataWordArray != null)
+            {
+                messageArray[0] = CommandWord;
+                DataWordArray.CopyTo(messageArray, 1);
+            }
+            else
+            {
+                messageArray[0] = CommandWord;
+            }
+            return messageArray;
+        }
+    }
+}
---
+++ deneme/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MILSTD1553ClassLibrary;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int commandWord = 123;
+            int[] dataWord = { 1, 1, 0, 1, 0, 1 };
+            int[] message;
+            float messageDuration;
+            Message msg = new Message(commandWord, dataWord);
+            message = msg.returnMessage();
+            messageDuration = msg.getMessageDuration();
+            while (true) ;
+        }
+    }
+}
